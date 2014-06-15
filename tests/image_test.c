f5dfba0b8b42406c70e4301c44af50dcf9198899
Sean Barrett <sean2@nothings.org>
2014-06-15 13:17:04 -0700

    Update stb_image test code to test loading with all channel counts (including 0), to avoid missing bugs like the recent TGA bug in the future. Doesn't check the *results*, but tests that it doesn't crash or fail.

--- tests/image_test.c
+++ tests/image_test.c
@@ -14,24 +14,34 @@ int main(int argc, char **argv)
       int i;
       for (i=1; i < argc; ++i) {
          unsigned char *data;
+         int n;
          printf("%s\n", argv[i]);
-         data = stbi_load(argv[i], &w, &h, 0, 4);
+         data = stbi_load(argv[i], &w, &h, &n, 4); if (data) free(data); else printf("Failed &n\n");
+         data = stbi_load(argv[i], &w, &h,  0, 1); if (data) free(data); else printf("Failed 1\n");
+         data = stbi_load(argv[i], &w, &h,  0, 2); if (data) free(data); else printf("Failed 2\n");
+         data = stbi_load(argv[i], &w, &h,  0, 3); if (data) free(data); else printf("Failed 3\n");
+         data = stbi_load(argv[i], &w, &h,  0, 4);
          assert(data);
          if (data) {
             char fname[512];
             stb_splitpath(fname, argv[i], STB_FILE);
             stbi_write_png(stb_sprintf("output/%s.png", fname), w, h, 4, data, w*4);
             free(data);
-         }
+         } else
+            printf("FAILED 4\n");
       }
    } else {
       int i;
       char **files = stb_readdir_files("images");
       for (i=0; i < stb_arr_len(files); ++i) {
+         int n;
          unsigned char *data;
          printf("%s\n", files[i]);
-         data = stbi_load(files[i], &w, &h, 0, 4);
-         //assert(data);
+         data = stbi_load(files[i], &w, &h, &n, 4); if (data) free(data); else printf("Failed &n\n");
+         data = stbi_load(files[i], &w, &h,  0, 1); if (data) free(data); else printf("Failed 1\n");
+         data = stbi_load(files[i], &w, &h,  0, 2); if (data) free(data); else printf("Failed 2\n");
+         data = stbi_load(files[i], &w, &h,  0, 3); if (data) free(data); else printf("Failed 3\n");
+         data = stbi_load(files[i], &w, &h,  0, 4);
          if (data) {
             char fname[512];
             stb_splitpath(fname, files[i], STB_FILE);
