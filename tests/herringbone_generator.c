88c99e47fb12a5d71cfe5f3eb6f7601357924fd4
Sean Barrett <sean2@nothings.org>
2014-07-04 08:06:36 -0700

    initial commit

---
+++ tests/herringbone_generator.c
@@ -0,0 +1,8 @@
+#define STB_HERRINGBONE_WANG_TILE_IMPLEMENTATION
+#include "stb_herringbone_wang_tile.h"
+
+int main(int argc, char **argv)
+{
+   return 0;
+}
+
--- tests/test_cpp_compilation.cpp
+++ tests/test_cpp_compilation.cpp
@@ -5,6 +5,7 @@
 #define STB_C_LEXER_IMPLEMENTATIOn
 #define STB_DIVIDE_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
+#define STB_HERRINGBONE_WANG_TILE_IMPLEMENTATION
 
 #include "stb_truetype.h"
 #include "stb_image_write.h"
@@ -13,5 +14,4 @@
 #include "stb_c_lexer.h"
 #include "stb_divide.h"
 #include "stb_image.h"
-
-
+#include "stb_herringbone_wang_tile.h"
