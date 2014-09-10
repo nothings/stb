671f04bd7e93dcdd91147bd33e19e476f5cc6247
Sean Barrett <sean2@nothings.org>
2014-09-10 15:35:06 -0700

    debugging truetype crash

--- tests/test_truetype.c
+++ tests/test_truetype.c
@@ -4,13 +4,26 @@
 #include <stdio.h>
 
 char ttf_buffer[1<<25];
+unsigned char output[512*100];
 
 #ifdef TT_TEST
+
+void debug(void)
+{
+   stbtt_fontinfo font;
+   fread(ttf_buffer, 1, 1<<25, fopen("c:/x/lm/LiberationMono-Regular.ttf", "rb"));
+   stbtt_InitFont(&font, ttf_buffer, 0);
+
+   stbtt_MakeGlyphBitmap(&font, output, 6, 9, 512, 5.172414E-03f, 5.172414E-03f, 54);
+}
+
 int main(int argc, char **argv)
 {
    stbtt_fontinfo font;
    unsigned char *bitmap;
    int w,h,i,j,c = (argc > 1 ? atoi(argv[1]) : 34807), s = (argc > 2 ? atoi(argv[2]) : 32);
+
+   debug();
 
    fread(ttf_buffer, 1, 1<<25, fopen(argc > 3 ? argv[3] : "c:/windows/fonts/mingliu.ttc", "rb"));
 
