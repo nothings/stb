2c56e11c59c8373b81b0bb49562fe88cba088d4e
Sean Barrett <sean2@nothings.org>
2014-11-24 18:21:36 -0800

    doc tweaks

--- stb_rect_pack.h
+++ stb_rect_pack.h
@@ -110,13 +110,13 @@ STBRP_DEF void stbrp_init_target (stbrp_context *context, int width, int height,
 STBRP_DEF void stbrp_setup_allow_out_of_mem (stbrp_context *context, int allow_out_of_mem);
 // Optionally call this function after init but before doing any packing to
 // change the handling of the out-of-temp-memory scenario, described above.
-// If you call init again, this will be reset.
+// If you call init again, this will be reset to the default (false).
 
 
 STBRP_DEF void stbrp_setup_heuristic (stbrp_context *context, int heuristic);
 // Optionally select which packing heuristic the library should use. Different
 // heuristics will produce better/worse results for different data sets.
-// If you call init again, this will be reset.
+// If you call init again, this will be reset to the default.
 
 enum
 {
