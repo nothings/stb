f22efc6151e4855b03b739845d34ab28f62483a0
Sean Barrett <sean2@nothings.org>
2015-04-12 09:26:16 -0700

    update credits/docs

--- stb_image.h
+++ stb_image.h
@@ -144,6 +144,7 @@
 
    Latest revision history:
       2.03  (2015-04-12) additional corruption checking
+                         stbi_set_flip_vertically_on_load
       2.02  (2015-01-19) fix incorrect assert, fix warning
       2.01  (2015-01-17) fix various warnings
       2.00b (2014-12-25) fix STBI_MALLOC in progressive JPEG
@@ -198,6 +199,7 @@
                                                  Cass Everitt
                                                  Engin Manap
                                                  Martins Mozeiko
+                                                 Joseph Thomson
 
 License:
    This software is in the public domain. Where that dedication is not
@@ -6265,7 +6267,8 @@ STBIDEF int stbi_info_from_callbacks(stbi_io_callbacks const *c, void *user, int
 
 /*
    revision history:
-      2.03  (2015-04-12) add extra corruption checking (mmozeiko)
+      2.03  (2015-04-12) extra corruption checking (mmozeiko)
+                         stbi_set_flip_vertically_on_load (nguillemot)
       2.02  (2015-01-19) fix incorrect assert, fix warning
       2.01  (2015-01-17) fix various warnings; suppress SIMD on gcc 32-bit without -msse2
       2.00b (2014-12-25) fix STBI_MALLOC in progressive JPEG
