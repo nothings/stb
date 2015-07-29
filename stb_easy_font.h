5305ffbe49b3b993f33b6251b07a116ceafeddac
Joshua Sheard <mail@jsheard.com>
2015-07-29 17:00:44 +0000

    Include math.h for ceil()

--- stb_easy_font.h
+++ stb_easy_font.h
@@ -88,6 +88,7 @@ void print_string(float x, float y, char *text, float r, float g, float b)
 #define INCLUDE_STB_EASY_FONT_H
 
 #include <stdlib.h>
+#include <math.h>
 
 struct {
     unsigned char advance;
