320b9299a4d34a9246e72789fea80449ac8d3527
Sarvadi <lrkulak@gmail.com>
2015-11-20 18:58:26 -0800

    Fix MSVC warnings in stb_image_write
    Macro redefinition

--- stb_image_write.h
+++ stb_image_write.h
@@ -143,9 +143,15 @@ STBIWDEF int stbi_write_hdr_to_func(stbi_write_func *func, void *context, int w,
 
 #ifdef STB_IMAGE_WRITE_IMPLEMENTATION
 
-#ifdef _WIN32
-   #define _CRT_SECURE_NO_WARNINGS
-   #define _CRT_NONSTDC_NO_DEPRECATE
+#if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
+//MSVC complains if either are already defined
+#pragma warning(push)
+#pragma warning(disable: 4005)
+#endif
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+#if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
+#pragma warning(pop)
 #endif
 
 #ifndef STBI_WRITE_NO_STDIO
