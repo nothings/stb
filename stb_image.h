472c4c82a44378b64245b1fbeaa9849f2cd0c51b
Sean Barrett <sean2@nothings.org>
2016-01-16 10:00:19 -0800

    update version history

--- stb_image.h
+++ stb_image.h
@@ -6458,8 +6458,10 @@ STBIDEF int stbi_info_from_callbacks(stbi_io_callbacks const *c, void *user, int
    revision history:
       2.09  (2016-01-16) allow comments in PNM files
                          16-bit-per-pixel TGA (not bit-per-component)
+                         info() for TGA could break due to .hdr handling
                          info() for BMP to shares code instead of sloppy parse
                          can use STBI_REALLOC_SIZED if allocator doesn't support realloc
+                         code cleanup
       2.08  (2015-09-13) fix to 2.07 cleanup, reading RGB PSD as RGBA
       2.07  (2015-09-13) fix compiler warnings
                          partial animated GIF support
