ee6978cb6891632e3fe84264f832140e590b43f0
Craig Donner <cdonner@google.com>
2016-02-10 14:48:59 -0800

    Slightly modify the public domain license to keep it in the public domain, but make it clear that even when dedications might not be recognized that the code is still usable.  Given that this isn't dual-licensing under a different license, I'm hoping this will be acceptable.

--- stb_perlin.h
+++ stb_perlin.h
@@ -3,9 +3,9 @@
 //
 // LICENSE
 //
-//   This software is in the public domain. Where that dedication is not
-//   recognized, you are granted a perpetual, irrevocable license to copy,
-//   distribute, and modify this file as you see fit.
+//   This software is dual-licensed to the public domain and under the following
+//   license: you are granted a perpetual, irrevocable license to copy, modify,
+//   publish, and distribute this file as you see fit.
 //
 //
 // to create the implementation,
