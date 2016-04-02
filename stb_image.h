7a694bdccafb2995936a81dd1ab27065da514019
Sean Barrett <sean2@nothings.org>
2016-04-02 04:01:59 -0700

    re-enable SSE2 code on x64 except with gcc

--- stb_image.h
+++ stb_image.h
@@ -673,7 +673,7 @@ typedef unsigned char validate_uint32[sizeof(stbi__uint32)==4 ? 1 : -1];
 #define STBI_NO_SIMD
 #endif
 
-#if !defined(STBI_NO_SIMD) && defined(STBI__X86_TARGET)
+#if !defined(STBI_NO_SIMD) && (defined(STBI__X86_TARGET) || defined(STBI__X64_TARGET))
 #define STBI_SSE2
 #include <emmintrin.h>
 
