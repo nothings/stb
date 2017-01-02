d0b576c47446a0ca549aa8aa6c98d66fcc9a06d8
Sean Barrett <sean@nothings.org>
2017-01-02 15:12:18 -0800

    update stb_truetype version

--- stb_truetype.h
+++ stb_truetype.h
@@ -1,4 +1,4 @@
-// stb_truetype.h - v1.12 - public domain
+// stb_truetype.h - v1.13 - public domain
 // authored from 2009-2016 by Sean Barrett / RAD Game Tools
 //
 //   This library processes TrueType files:
@@ -20,6 +20,7 @@
 //
 //   Mikko Mononen: compound shape support, more cmap formats
 //   Tor Andersson: kerning, subpixel rendering
+//   Dougall Johnson: OpenType / Type 2 font handling
 //
 //   Misc other:
 //       Ryan Gordon
