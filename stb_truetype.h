3d1c790e96c9f35eeabdff5b33c8fc21b81277c5
Sean Barrett <sean@nothings.org>
2017-01-02 15:16:22 -0800

    version history

--- stb_truetype.h
+++ stb_truetype.h
@@ -52,6 +52,7 @@
 //
 // VERSION HISTORY
 //
+//   1.13 (2017-01-02) support OpenType fonts
 //   1.12 (2016-10-25) suppress warnings about casting away const with -Wcast-qual
 //   1.11 (2016-04-02) fix unused-variable warning
 //   1.10 (2016-04-02) user-defined fabs(); rare memory leak; remove duplicate typedef
