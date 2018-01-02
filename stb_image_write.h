841862a6225c891d32d194267ba4b81f1499755a
John Tullos <xeek@xeekworx.com>
2018-01-01 18:56:36 -0600

    Fixed grammar, spelling issues in comments issue #533

--- stb_image_write.h
+++ stb_image_write.h
@@ -10,8 +10,8 @@
 
    Will probably not work correctly with strict-aliasing optimizations.
 
-   If using a modern Microsoft Compiler non-safe versions of CRT calls may cause 
-   compilation warnings or even errors. To avoid this, aldo before #including,
+   If using a modern Microsoft Compiler, non-safe versions of CRT calls may cause 
+   compilation warnings or even errors. To avoid this, also before #including,
 
        #define STBI_MSC_SECURE_CRT
 
