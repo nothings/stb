9b2522c3d53b16cefa99af660e1d0079cbaf4a9e
Sean Barrett <sean@nothings.org>
2018-01-29 07:48:12 -0800

    stbi_is_hdr_from_file needs to reset file position

--- stb_image.h
+++ stb_image.h
@@ -1333,9 +1333,12 @@ STBIDEF int      stbi_is_hdr          (char const *filename)
 STBIDEF int      stbi_is_hdr_from_file(FILE *f)
 {
    #ifndef STBI_NO_HDR
+   long pos = ftell(f), res;
    stbi__context s;
    stbi__start_file(&s,f);
-   return stbi__hdr_test(&s);
+   res = stbi__hdr_test(&s);
+   fseek(f, pos, SEEK_SET);
+   return res;
    #else
    STBI_NOTUSED(f);
    return 0;
