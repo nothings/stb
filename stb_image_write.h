c8d4ee8591a36dc2bada65455a5ff5c62a099bc8
Sean Barrett <sean@nothings.org>
2018-02-11 11:11:39 -0800

    stb_image_write: credits

--- stb_image_write.h
+++ stb_image_write.h
@@ -134,6 +134,8 @@ CREDITS:
       Patrick Boettcher
       github:xeekworx
       Cap Petschulat
+      Simon Rodriguez
+      Ivan Tikhonov
 
 LICENSE
 
@@ -234,7 +236,7 @@ STBIWDEF void stbi_flip_vertically_on_write(int flip_boolean);
 #define STBIW_UCHAR(x) (unsigned char) ((x) & 0xff)
 
 #ifdef STB_IMAGE_WRITE_STATIC
-static stbi__flip_vertically_on_write=0;
+static int stbi__flip_vertically_on_write=0;
 static int stbi_write_png_compression_level = 8;
 static int stbi_write_tga_with_rle = 1;
 static int stbi_write_force_png_filter = -1;
