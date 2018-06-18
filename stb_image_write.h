d20dfa588805be565f99ac15ff770feb434fa7eb
James <jmarshallstewart@gmail.com>
2018-06-17 21:30:43 -0500

    Add sizeOfBuffer argument for sprintf_s when STBI_MSC_SECURE_CRT is defined.

--- stb_image_write.h
+++ stb_image_write.h
@@ -687,7 +687,7 @@ static int stbi_write_hdr_core(stbi__write_context *s, int x, int y, int comp, f
       s->func(s->context, header, sizeof(header)-1);
 
 #ifdef STBI_MSC_SECURE_CRT
-      len = sprintf_s(buffer, "EXPOSURE=          1.0000000000000\n\n-Y %d +X %d\n", y, x);
+      len = sprintf_s(buffer, sizeof(buffer), "EXPOSURE=          1.0000000000000\n\n-Y %d +X %d\n", y, x);
 #else
       len = sprintf(buffer, "EXPOSURE=          1.0000000000000\n\n-Y %d +X %d\n", y, x);
 #endif
