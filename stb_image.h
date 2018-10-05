dd23e6c5e4774121a9b3aba657d78c15f8fcbf10
Vasu Mahesh <vasu.mahesh@hotmail.com>
2018-10-05 18:08:38 -0400

    fix warnings for unreferenced formal parameter in stb_image

--- stb_image.h
+++ stb_image.h
@@ -101,6 +101,7 @@ RECENT REVISION HISTORY:
     Julian Raschke          Gregory Mullen     Baldur Karlsson    github:poppolopoppo
     Christian Floisand      Kevin Schmidt                         github:darealshinji
     Blazej Dariusz Roszkowski                                     github:Michaelangel007
+    Vasu Mahesh
 */
 
 #ifndef STBI_INCLUDE_STB_IMAGE_H
@@ -6338,6 +6339,8 @@ static stbi_uc *stbi__process_gif_raster(stbi__context *s, stbi__gif *g)
 // two back is the image from two frames ago, used for a very specific disposal format
 static stbi_uc *stbi__gif_load_next(stbi__context *s, stbi__gif *g, int *comp, int req_comp, stbi_uc *two_back)
 {
+   STBI_NOTUSED(req_comp);
+
    int dispose; 
    int first_frame; 
    int pi; 
@@ -6562,6 +6565,8 @@ static void *stbi__load_gif_main(stbi__context *s, int **delays, int *x, int *y,
 
 static void *stbi__gif_load(stbi__context *s, int *x, int *y, int *comp, int req_comp, stbi__result_info *ri)
 {
+   STBI_NOTUSED(ri);
+
    stbi_uc *u = 0;
    stbi__gif g;
    memset(&g, 0, sizeof(g));
