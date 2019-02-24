57c7029166d3c2d94f2905bd3371d5307435f927
Sean Barrett <sean@nothings.org>
2019-02-24 04:22:47 -0800

    test_image_write.c: change from standalone to integrated with image_test.c

--- tests/test_image.c
+++ tests/test_image.c
@@ -1,1 +1,3 @@
+#define STB_IMAGE_STATIC
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
--- tests/test_image_write.c
+++ tests/test_image_write.c
@@ -1,1 +1,3 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_STATIC
 #include "stb_image_write.h"
