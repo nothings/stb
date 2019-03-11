06374082843275af71e1ccc4ca28e13fe3e51ed7
Randy <randy408@protonmail.com>
2019-03-12 00:59:20 +0100

    fuzz: fix error handling

--- tests/stb_png_read_fuzzer.cpp
+++ tests/stb_png_read_fuzzer.cpp
@@ -6,7 +6,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     int x, y, channels;
 
-    if(stbi_info_from_memory(data, size, &x, &y, &channels)) return 0;
+    if(!stbi_info_from_memory(data, size, &x, &y, &channels)) return 0;
 
     /* exit if the image is larger than ~80MB */
     if(y && x > (80000000 / 4) / y) return 0;
