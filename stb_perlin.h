a82e7424d20623c7fdf64051e5c3bf62b1f0ae80
fluffrabbit <fluffrabbit@aol.com>
2019-08-03 19:15:41 -0600

    extern stb_perlin_noise3_seed

--- stb_perlin.h
+++ stb_perlin.h
@@ -78,6 +78,7 @@
 extern "C" {
 #endif
 extern float stb_perlin_noise3(float x, float y, float z, int x_wrap, int y_wrap, int z_wrap);
+extern float stb_perlin_noise3_seed(float x, float y, float z, int x_wrap, int y_wrap, int z_wrap, int seed);
 extern float stb_perlin_ridge_noise3(float x, float y, float z, float lacunarity, float gain, float offset, int octaves);
 extern float stb_perlin_fbm_noise3(float x, float y, float z, float lacunarity, float gain, int octaves);
 extern float stb_perlin_turbulence_noise3(float x, float y, float z, float lacunarity, float gain, int octaves);
