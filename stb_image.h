8cb98357dec364739926b52f20fdd1e3b91c61c8
Recep Aslantas <info@recp.me>
2020-05-26 00:03:46 +0300

    stbi: fix thread local selector
    * GCC < 5 supports __thread and GCC >= 5 supports C11 with _Thread_local
    * Skip _Thread_local for MSVC because it may not be supported

--- stb_image.h
+++ stb_image.h
@@ -574,13 +574,13 @@ STBIDEF int   stbi_zlib_decode_noheader_buffer(char *obuffer, int olen, const ch
 #ifndef STBI_NO_THREAD_LOCALS
    #if defined(__cplusplus) &&  __cplusplus >= 201103L
       #define STBI_THREAD_LOCAL       thread_local
-   #elif defined (__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
-      #define STBI_THREAD_LOCAL       _Thread_local
-   #elif defined(__GNUC__)
+   #elif defined(__GNUC__) && __GNUC__ < 5
       #define STBI_THREAD_LOCAL       __thread
    #elif defined(_MSC_VER)
       #define STBI_THREAD_LOCAL       __declspec(thread)
-#endif
+   #elif defined (__STDC_VERSION__) && __STDC_VERSION__ >= 201112L && !defined(__STDC_NO_THREADS__)
+      #define STBI_THREAD_LOCAL       _Thread_local
+   #endif
 #endif
 
 #ifdef _MSC_VER
