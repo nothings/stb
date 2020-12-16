f0f253375415d500165ae59fe81d190f028bcd1c
AnthoFoxo <anthofoxo@gmail.com>
2020-12-16 18:19:03 -0500

    closes #1063; Fixed files with no comments emitting outofmemory errors

--- stb_vorbis.c
+++ stb_vorbis.c
@@ -3643,7 +3643,7 @@ static int start_decoder(vorb *f)
    //user comments
    f->comment_list_length = get32_packet(f);
    f->comment_list = (char**)setup_malloc(f, sizeof(char*) * (f->comment_list_length));
-   if (f->comment_list == NULL)                     return error(f, VORBIS_outofmem);
+   if (f->comment_list == NULL && f->comment_list_length > 0)       return error(f, VORBIS_outofmem);
 
    for(i=0; i < f->comment_list_length; ++i) {
       len = get32_packet(f);
