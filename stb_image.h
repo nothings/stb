41e93836d957fe4eca5a59de466d5cbcdbf43713
Fabian Giesen <rygorous@gmail.com>
2021-07-02 21:57:25 -0700

    stb_image: UB fix in stbi__get32le
    Need to do the second-part shift on uint32 not int.

--- stb_image.h
+++ stb_image.h
@@ -1674,7 +1674,8 @@ static int stbi__get16le(stbi__context *s)
 static stbi__uint32 stbi__get32le(stbi__context *s)
 {
    stbi__uint32 z = stbi__get16le(s);
-   return z + (stbi__get16le(s) << 16);
+   z += (stbi__uint32)stbi__get16le(s) << 16;
+   return z;
 }
 #endif
 
