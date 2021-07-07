afc9c16bfd5be9ce81e0e0e1255b47d38218c773
Fabian Giesen <rygorous@gmail.com>
2021-07-07 03:43:15 -0700

    stb_truetype: Change spelling of fallthrough comment
    To pacify GCC warnings at -Wimplicit-fallthrough=4. Why the
    all-caps version works and the others don't, I'm not sure; the
    GCC manual page lists regular expressions that GCC is checking for
    but does not say which regular expression syntax variant it's using,
    whether it's looking for a substring match or a full match for
    the comment, and what exactly the text being matched against is
    for a single-line comment. Sigh.
    
    Fixes issue #507.

--- stb_truetype.h
+++ stb_truetype.h
@@ -2136,7 +2136,7 @@ static int stbtt__run_charstring(const stbtt_fontinfo *info, int glyph_index, st
                subrs = stbtt__cid_get_glyph_subrs(info, glyph_index);
             has_subrs = 1;
          }
-         // fallthrough
+         // FALLTHROUGH
       case 0x1D: // callgsubr
          if (sp < 1) return STBTT__CSERR("call(g|)subr stack");
          v = (int) s[--sp];
