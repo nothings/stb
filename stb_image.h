9b77e0b5140fb3e5d09d95527adad9f0ea6335f8
Weston McNamara <westonfmcnamara@gmail.com>
2022-02-18 15:29:15 -0500

    Added /*fallthrough*/ comments after macro

--- stb_image.h
+++ stb_image.h
@@ -398,7 +398,7 @@ extern "C" {
 
 //C++17 fallthrough macro
 #if (__cplusplus >= 201703L) || (defined(_MSVC_LANG) && (_MSVC_LANG >= 201703L) && (_MSC_VER >= 1913)) 
-#define STBI_FALLTHROUGH [[fallthrough]]; /* fallthrough */
+#define STBI_FALLTHROUGH [[fallthrough]];
 #else
 #define STBI_FALLTHROUGH
 #endif
@@ -5673,10 +5673,10 @@ static int stbi__tga_get_comp(int bits_per_pixel, int is_grey, int* is_rgb16)
    switch(bits_per_pixel) {
       case 8:  return STBI_grey;
       case 16: if(is_grey) return STBI_grey_alpha;
-               STBI_FALLTHROUGH
+               STBI_FALLTHROUGH /*fallthrough*/
       case 15: if(is_rgb16) *is_rgb16 = 1;
                return STBI_rgb;
-      case 24: STBI_FALLTHROUGH
+      case 24: STBI_FALLTHROUGH /*fallthrough*/
       case 32: return bits_per_pixel/8;
       default: return 0;
    }
@@ -7075,10 +7075,10 @@ static void stbi__hdr_convert(float *output, stbi_uc *input, int req_comp)
       if (req_comp == 4) output[3] = 1;
    } else {
       switch (req_comp) {
-      case 4: output[3] = 1; STBI_FALLTHROUGH
+      case 4: output[3] = 1; STBI_FALLTHROUGH /*fallthrough*/
          case 3: output[0] = output[1] = output[2] = 0;
                  break;
-         case 2: output[1] = 1; STBI_FALLTHROUGH
+         case 2: output[1] = 1; STBI_FALLTHROUGH /*fallthrough*/
          case 1: output[0] = 0;
                  break;
       }
