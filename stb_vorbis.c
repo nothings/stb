c934a0c5dff87f06316b89a623b7d2fe77d31bdf
Seb-degraff <seb.degraff@gmail.com>
2023-10-22 13:59:45 +0200

    fix undefined behaviour in stb_vorbis.c Fixes distorted decompressed audio when UBSan was enabled.

--- stb_vorbis.c
+++ stb_vorbis.c
@@ -2067,7 +2067,7 @@ static __forceinline void draw_line(float *output, int x0, int y0, int x1, int y
    ady -= abs(base) * adx;
    if (x1 > n) x1 = n;
    if (x < x1) {
-      LINE_OP(output[x], inverse_db_table[y&255]);
+      LINE_OP(output[x], inverse_db_table[(uint32)y&255]);
       for (++x; x < x1; ++x) {
          err += ady;
          if (err >= adx) {
@@ -2075,7 +2075,7 @@ static __forceinline void draw_line(float *output, int x0, int y0, int x1, int y
             y += sy;
          } else
             y += base;
-         LINE_OP(output[x], inverse_db_table[y&255]);
+         LINE_OP(output[x], inverse_db_table[(uint32)y&255]);
       }
    }
 }
