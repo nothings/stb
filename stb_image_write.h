892d73e5d0ba4906149809c964cfd74e9da81a52
Giovanni Bajo <giovannibajo@gmail.com>
2024-05-01 11:36:38 +0200

    Fix warning on use of sprintf
    Using snprintf avoid compilation warnings/errors on modern UNIX systems. Specifically, this triggered on macOS 14.4.1.

--- stb_image_write.h
+++ stb_image_write.h
@@ -773,7 +773,7 @@ static int stbi_write_hdr_core(stbi__write_context *s, int x, int y, int comp, f
 #ifdef __STDC_LIB_EXT1__
       len = sprintf_s(buffer, sizeof(buffer), "EXPOSURE=          1.0000000000000\n\n-Y %d +X %d\n", y, x);
 #else
-      len = sprintf(buffer, "EXPOSURE=          1.0000000000000\n\n-Y %d +X %d\n", y, x);
+      len = snprintf(buffer, sizeof(buffer), "EXPOSURE=          1.0000000000000\n\n-Y %d +X %d\n", y, x);
 #endif
       s->func(s->context, buffer, len);
 
