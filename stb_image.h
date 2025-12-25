19abaed60dcf585cb9fbe952282f46db3ce47c23
Nia Bickford <nbickford@nvidia.com>
2025-12-24 23:14:40 -0800

    In stbi__start_mem, set s->io.skip to NULL in stbi__start_mem so that https://github.com/nothings/stb/pull/1751 doesn't segfault when `io.skip` is nonzero and uninitialized. Also set s->io.eof to NULL for good measure.

--- stb_image.h
+++ stb_image.h
@@ -826,6 +826,8 @@ static void stbi__refill_buffer(stbi__context *s);
 static void stbi__start_mem(stbi__context *s, stbi_uc const *buffer, int len)
 {
    s->io.read = NULL;
+   s->io.skip = NULL;
+   s->io.eof = NULL;
    s->read_from_callbacks = 0;
    s->callback_already_read = 0;
    s->img_buffer = s->img_buffer_original = (stbi_uc *) buffer;
